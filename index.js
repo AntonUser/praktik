dafabc406649ccd9e976b573d9e3f5d4bc97b808
Anton <titovanton5@mail.ru>
2020-07-09 22:10:01 +0600

    V 1.3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // подключение express
 const express = require("express");
 const bodyParser = require('body-parser');
+const valid = require("./validate_arguments.js");
 // создаем объект приложения
 const app = express();
 // Подключаем body-parser
@@ -8,80 +9,42 @@ app.use(bodyParser.json());
 
 // Определяем обработчик корневого урла с методом POST
 app.post("/", function (request, response) {
-    const { firstArg, secondArg, operation } = request.body; // деструктуризация
-    if (operation === '+') {
-        response.status(200).json(firstArg + secondArg);
-    } else if (operation === '-') {
-        response.status(200).json(firstArg - secondArg);
-    } else if (operation === '/') {
-        if (secondArg === 0) {
-            response.status(200).json('Деление на 0 невозможно!')
-        }
-        response.status(200).json(firstArg / secondArg);
-    } else if (operation === '*') {
-        response.status(200).json(firstArg * secondArg);
-    } else if (operation === 'pow') {
-        response.status(200).json(Math.pow(firstArg, secondArg));//secondArg - степень
-    } else if (operation === 'sin') {
-        if (secondArg === 'deg') {
-            response.status(200).json(Math.sin(firstArg * Math.PI / 180));//в градусы
-        } else {
-            response.status(200).json(Math.sin(firstArg));
-        }
-    } else if (operation === 'cos') {
-        if (secondArg === 'deg') {
-            response.status(200).json(Math.cos(firstArg * Math.PI / 180));//в градусы
-        } else {
-            response.status(200).json(Math.cos(firstArg));
-        }
-    } else if (operation === 'tan') {
-        if (secondArg === 'deg') {
-            if (firstArg === 90 || firstArg === 270) {
-                response.status(200).json('Для заданного угла тангенс отсутствует');
-            } else {
-                response.status(200).json(Math.tan(firstArg * Math.PI / 180));//в градусы
-            }
-        } else {
-            if (firstArg === (Math.PI / 2) || firstArg === (3 * Math.PI / 2)) {
-                response.status(200).json('Для заданного угла тангенс отсутствует');
-            } else {
-                response.status(200).json(Math.tan(firstArg));
-            }
-        }
 
+    const { firstArg, secondArg, operation } = request.body; // деструктуризация
+
+    if (operation === '+') {
+        response.status(200).json(valid.validateAdd(firstArg, secondArg));
+    } else if (operation === '-') {
+        response.status(200).json(valid.validateDiff(firstArg, secondArg));
+    } else if (operation === '/') {
+        response.status(200).json(valid.validateDiv(firstArg, secondArg));
+    } else if (operation === '*') {
+        response.status(200).json(valid.validateMul(firstArg, secondArg));
+    } else if (operation === 'pow') {
+        response.status(200).json(valid.validatePow(firstArg, secondArg));//secondArg - степень
+    } else if (operation === 'sin') {
+        response.status(200).json(valid.validateSin(firstArg, secondArg));
+    } else if (operation === 'cos') {
+        response.status(200).json(valid.validateCos(firstArg, secondArg));
+    } else if (operation === 'tan') {
+        response.status(200).json(valid.validateTan(firstArg, secondArg));
     } else if (operation === 'ctan') {
-        if (secondArg === 'deg') {
-            if (firstArg === 0 || firstArg === 180 || firstArg === 360) {
-                response.status(200).json('Для заданного угла тангенс отсутствует');
-            }
-            else {
-                response.status(200).json((1 / Math.tg(firstArg * Math.PI / 180)));//в градусы
-            }
-        } else {
-            if (firstArg === 0 || firstArg === Math.PI || firstArg === Math.PI * 2) {
-                response.status(200).json('Для заданного угла тангенс отсутствует');
-            }
-            else {
-                response.status(200).json(1 / Math.tg(firstArg));
-            }
-        }
+        response.status(200).json(valid.validateCtan(firstArg, secondArg));
     } else if (operation === 'per') {//процент
-        if (secondArg < 0 || secondArg > 100) {
-            response.status(200).json('Процент выходит за границы допустимых значений');
-        } else {
-            response.status(200).json(firstArg * secondArg / 100);
-        }
+        response.status(200).json(valid.validatePer(firstArg, secondArg));
     }
 });
 
-app.post("/oneArg", function (request, response) {//с напарником посоветовались, но в итоге не пришли к выводу 
-    //правильное ли это решение- отдельный маршрут для операций с одним аргументом
+app.post("/oneArg", function (request, response) {
+
     const { firstArg, operation } = request.body;
+    
     if (operation === 'sqrt') {
-        response.status(200).json(Math.sqrt(firstArg));
+        response.status(200).json(valid.validateSqrt(firstArg));
     } else if (operation === 'sqr') {
-        response.status(200).json(Math.pow(firstArg, 2));//возведение в квадрат
+        response.status(200).json(valid.validateSqr(firstArg));//возведение в квадрат
     }
+    
 });
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3001);
---
+++ operations.js
@@ -0,0 +1,67 @@
+
+function addition(firstArg, secondArg) {
+    return firstArg + secondArg;
+}
+
+function difference(firstArg, secondArg) {
+    return firstArg - secondArg;
+}
+
+function multiplication(firstArg, secondArg) {
+    return firstArg * secondArg;
+}
+
+function division(firstArg, secondArg) {
+    return firstArg / secondArg;
+}
+
+function power(firstArg, secondArg) {
+    return Math.pow(firstArg, secondArg);
+}
+
+function sinusDeg(firstArg) {
+    return Math.sin(firstArg * Math.PI / 180);
+}
+
+function sinusRad(firstArg) {
+    return Math.sin(firstArg);
+}
+
+function cosinusDeg(firstArg) {
+    return Math.cos(firstArg * Math.PI / 180);
+}
+
+function cosinusRad(firstArg) {
+    return Math.cos(firstArg);
+}
+
+function tangensDeg(firstArg) {
+    return Math.tan(firstArg * Math.PI / 180);
+}
+
+function tangensRad(firstArg) {
+    return Math.tan(firstArg);
+}
+
+function percent(firstArg, secondArg) {
+    return firstArg * secondArg / 100;
+}
+
+function sqrt(firstArg) {
+    return Math.sqrt(firstArg);
+}
+module.exports = {
+    addition: addition,
+    difference: difference,
+    multiplication: multiplication,
+    division: division,
+    power: power,
+    sinusDeg: sinusDeg,
+    sinusRad: sinusRad,
+    cosinusDeg: cosinusDeg,
+    cosinusRad: cosinusRad,
+    tangensDeg: tangensDeg,
+    tangensRad: tangensRad,
+    percent: percent,
+    sqrt: sqrt
+};
